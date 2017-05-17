85d6519d91135f8683a343d8429b656024d3ac88
FelixSeol <felixxseol@gmail.com>
2017-05-18 01:45:58 +0900

    Init GUI

--- src/Controller/SimpleMergeDriver.java
+++ src/Controller/SimpleMergeDriver.java
@@ -1,8 +1,69 @@
 package Controller;
 
-public class SimpleMergeDriver {
-	public static void main(String[] args){
-		System.out.println("Drive in");
-		
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class SimpleMergeDriver extends Application {
+
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("SimpleMerge");
+
+        initRootLayout();
+
+        showSimpleMerge();
+    }
+
+    /**
+     * 상위 레이아웃을 초기화한다.
+     */
+    public void initRootLayout() {
+        try {
+            // fxml 파일에서 상위 레이아웃을 가져온다.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(SimpleMergeDriver.class.getResource("../View/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // 상위 레이아웃을 포함하는 scene을 보여준다.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 상위 레이아웃 안에 연락처 요약(person overview)을 보여준다.
+     */
+    public void showSimpleMerge() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(SimpleMergeDriver.class.getResource("../View/FrameLayout.fxml"));
+            AnchorPane FrameLayout = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(FrameLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
