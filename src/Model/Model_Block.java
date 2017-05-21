347033d93e90ed85b75cb6f2ea87d8e96f8c84b2
jooman <kohjum2@gmail.com>
2017-05-21 22:02:27 +0900

    delete simpleMergeDrive

--- src/Model/Model_Block.java
+++ src/Model/Model_Block.java
@@ -7,24 +7,21 @@ public class Model_Block {
 	 * blank : 블럭에 공백 줄  수
 	 * modified: merge를 통해 수정이 됐는지
 	 * */
-	private int startIndex;
-	private int lastIndex;
+	private ArrayList<String> lineInfo;
+	public ArrayList<String> getLineInfo() {
+		return lineInfo;
+	}
+	public void setLineInfo(ArrayList<String> lineInfo) {
+		this.lineInfo = lineInfo;
+	}
 	private int blank;
-	private boolean modified;
+	private boolean modified = false;
 	private boolean isSame = false;
 	
-	public Model_Block(int startIndex,int lastIndex,int blank){
-		this.startIndex = startIndex;
-		this.lastIndex = lastIndex;
+	public Model_Block(int blank){
 		this.blank = blank;
 	}
 	//getter
-	public int getStartIndex(){
-		return startIndex;
-	}
-	public int getLastIndex(){
-		return lastIndex;
-	}
 	public int getBlank(){
 		return blank;
 	}
@@ -35,12 +32,6 @@ public class Model_Block {
 		return isSame;
 	}
 	//setter
-	public void setStartIndex(int startIndex){
-		this.startIndex = startIndex;
-	}
-	public void setLastIndex(int lastIndex){
-		this.lastIndex = lastIndex;
-	}
 	public void setBlank(int blank){
 		this.blank = blank;
 	}
